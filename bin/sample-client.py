33929d94d0e8379c182d86068e15c1356548a1a1
Benjamin Saller <bcsaller@objectrealms.net>
2007-05-07 02:53:15 -0700

    git inexperience

--- bin/sample-client.py
+++ bin/sample-client.py
@@ -13,28 +13,5 @@ def main():
     uid = remote_object.create(dict(title="from dbus"), '/etc/passwd')
     print "created uid", uid
 
-##     # ... or create an Interface wrapper for the remote object
-##     iface = dbus.Interface(remote_object, "com.example.SampleInterface")
-
-##     hello_reply_tuple = iface.GetTuple()
-
-##     print hello_reply_tuple
-
-##     hello_reply_dict = iface.GetDict()
-
-##     print hello_reply_dict
-
-##     # D-Bus exceptions are mapped to Python exceptions
-##     try:
-##         iface.RaiseException()
-##     except dbus.DBusException, e:
-##         print str(e)
-
-    # introspection is automatically supported
-    print remote_object.Introspect(dbus_interface="org.freedesktop.DBus.Introspectable")
-
-    #if sys.argv[1:] == ['--exit-service']:
-    #    iface.Exit()
-
 if __name__ == '__main__':
     main()

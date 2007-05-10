7fdc5cfbf50870c78a7100b38a410c76b57c8461
Benjamin Saller <bcsaller@objectrealms.net>
2007-05-10 15:01:25 -0700

    removed automatic version tracking to support build system

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 from setuptools import setup, find_packages
-import sys, os
 
-sys.path.insert(0,
-                os.path.join(os.path.dirname(__file__),'src')) # for version
-
-
-from olpc.datastore.__version__ import version
+#import sys, os
+#sys.path.insert(0, os.path.join(os.path.dirname(__file__),'src')) # for version
+#from olpc.datastore.__version__ import version
 
 DEPENDS = ['sqlalchemy >= 0.3.6', ]
 
 
 setup(
-    name="olpc.datastore",
-    version=version.v_short,
+    name="sugar-datastore",
+    version="0.2.0",
     packages=find_packages('src', exclude=["*.tests"]),
     package_dir= {'':'src'},
     namespace_packages=['olpc'],

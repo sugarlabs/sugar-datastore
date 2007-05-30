8d46929bf71272441289b0866cb4f6fae3ed5dd7
Marco Pesenti Gritti <marco@localhost.localdomain>
2007-05-30 21:10:08 +0200

    Add a little scripts that I use for the builds.

---
+++ build-snapshot.sh
@@ -0,0 +1,11 @@
+VERSION=0.2.1
+ALPHATAG=`git-show-ref --hash=10 refs/heads/master`
+TARBALL=sugar-datastore-$VERSION-git$ALPHATAG.tar.bz2
+
+rm sugar-datastore-$VERSION.tar.bz2
+
+make distcheck
+
+mv sugar-datastore-$VERSION.tar.bz2 $TARBALL
+scp $TARBALL mpg@devserv.devel.redhat.com:~
+rm $TARBALL

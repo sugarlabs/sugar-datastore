2d944f7c58508fde1fa196c8966fdb2a454c8c65
Marco Pesenti Gritti <marco@localhost.localdomain>
2007-06-20 10:48:43 +0200

    Update build snapshot script

--- build-snapshot.sh
+++
@@ -1,9 +0,0 @@
-VERSION=0.2.1
-ALPHATAG=`git-show-ref --hash=10 refs/heads/master`
-TARBALL=sugar-datastore-$VERSION-git$ALPHATAG.tar.bz2
-
-rm sugar-datastore-$VERSION.tar.bz2
-
-make distcheck
-
-mv sugar-datastore-$VERSION.tar.bz2 $TARBALL

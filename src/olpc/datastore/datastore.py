f6cac3e333dc0846d425e6caba845158fbd25aee
Benjamin Saller <bcsaller@objectrealms.net>
2007-10-09 08:34:11 -0700

    changes to api to reflect the DBus compat requests of #4064

--- src/olpc/datastore/datastore.py
+++ src/olpc/datastore/datastore.py
@@ -255,9 +255,9 @@ class DataStore(dbus.service.Object):
 
     #@utils.sanitize_dbus    
     @dbus.service.method(DS_DBUS_INTERFACE,
-             in_signature='a{sv}as',
+             in_signature='a{sv}',
              out_signature='aa{sv}u')
-    def find(self, query=None, properties=None, **kwargs):
+    def find(self, query=None, **kwargs):
         """find(query)
         takes a dict of parameters and returns data in the following
              format
@@ -294,7 +294,8 @@ class DataStore(dbus.service.Object):
         else:
             if 'query' not in kwargs:
                 kwargs['query'] = query
-        
+
+        properties = kwargs.pop("properties", [])
         include_files = kwargs.pop('include_files', False)
         order_by = kwargs.pop('order_by', [])
 

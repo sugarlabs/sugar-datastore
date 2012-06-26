ad546a1883d5c82e0ab4c1444d484c011188dc75
Daniel Narvaez <dwnarvaez@gmail.com>
2012-06-26 12:54:08 +0000

    Allow to build outside the source directory
    Reviewed-by: James Cameron <quozl@laptop.org>
    Acked-by: Simon Schampijer <simon@schampijer.de

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+
+test -n "${srcdir}" || srcdir=`dirname "$0"`
+test -n "${srcdir}" || srcdir="$(pwd)"
+
+olddir="$(pwd)"
+cd "$srcdir"
+
 export ACLOCAL="aclocal -I m4"
 
 autoreconf -i
-./configure --enable-maintainer-mode "$@"
+
+cd "$olddir"
+"$srcdir/configure" --enable-maintainer-mode "$@"

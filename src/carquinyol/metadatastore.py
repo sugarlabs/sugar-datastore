sugarlabs/sugar-datastore

commit 9e21c0ab9058ac5ac958a2052a9b35b04d379149

Port to Python 3

src/carquinyol/metadatastore.py
@@ -14,13 +14,13 @@ class MetadataStore(object):
         if not os.path.exists(metadata_path):
             os.makedirs(metadata_path)
         else:
-            received_keys = metadata.keys()
+            received_keys = list(metadata.keys())
             for key in os.listdir(metadata_path):
                 if key not in _INTERNAL_KEYS and key not in received_keys:
                     os.remove(os.path.join(metadata_path, key))
 
         metadata['uid'] = uid
-        for key, value in metadata.items():
+        for key, value in list(metadata.items()):
             self._set_property(uid, key, value, md_path=metadata_path)
 
     def _set_property(self, uid, key, value, md_path=False):
@@ -38,15 +38,12 @@ class MetadataStore(object):
         # str() is 8-bit clean right now, but
         # this won't last. We will need more explicit
         # handling of strings, int/floats vs raw data
-        if isinstance(value, unicode):
-            value = value.encode('utf-8')
-        elif not isinstance(value, basestring):
-            value = str(value)
-
+        if isinstance(value, bytes):
+            value = str(value)[2:-1]
         # avoid pointless writes; replace atomically
         if os.path.exists(fpath):
-            stored_val = open(fpath, 'r').read()
-
+            stored_val = open(fpath, 'rb').read()
+            stored_val = stored_val[2:-1]
             if stored_val == value:
                 changed = False
         if changed:
@@ -57,7 +54,12 @@ class MetadataStore(object):
 
     def retrieve(self, uid, properties=None):
         metadata_path = layoutmanager.get_instance().get_metadata_path(uid)
-        return metadatareader.retrieve(metadata_path, properties)
+        if properties is not None:
+            properties = [x.encode('utf-8') if isinstance(x,str) else x for x in properties]
+        metadata = metadatareader.retrieve(metadata_path, properties)
+        for x in metadata:
+            metadata[x] = str(metadata[x])[2:-1]
+        return metadata
 
     def delete(self, uid):
         metadata_path = layoutmanager.get_instance().get_metadata_path(uid)

bin/copy-from-journal
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 #
 # Simple script to export a file from the datastore
 # Reinier Heeres, <reinier@heeres.eu>, 2007-12-24
@@ -71,8 +71,8 @@ if __name__ == "__main__":
             objects, count = \
                     datastore.find(query, limit=RETURN_LIMIT, sorting='-mtime')
             if count > 1:
-                print 'WARNING: %d objects found; getting most recent.' % count
-                for i in xrange(1, RETURN_LIMIT):
+                print('WARNING: %d objects found; getting most recent.' % count)
+                for i in range(1, RETURN_LIMIT):
                     objects[i].destroy()
 
             if count > 0:
@@ -80,20 +80,20 @@ if __name__ == "__main__":
 
         # If neither an explicit object ID nor a query gave us data, fail.
         if dsentry is None:
-            print 'ERROR: unable to determine journal object to copy.'
+            print('ERROR: unable to determine journal object to copy.')
             argument_parser.print_help()
             exit(0)
 
         # Print metadata if that is what the user asked for.
         if args.show_meta:
-            print 'Metadata:'
-            for key, val in dsentry.metadata.get_dictionary().iteritems():
+            print('Metadata:')
+            for key, val in dsentry.metadata.get_dictionary().items():
                 if key != 'preview':
-                    print '%20s -> %s' % (key, val)
+                    print('%20s -> %s' % (key, val))
 
         # If no file is associated with this object, we can't save it out.
         if dsentry.get_file_path() == "":
-            print 'ERROR: no file associated with object, just metadata.'
+            print('ERROR: no file associated with object, just metadata.')
             dsentry.destroy()
             exit(0)
 
@@ -112,15 +112,15 @@ if __name__ == "__main__":
         # Lastly, actually copy the file out of the datastore and onto the
         # filesystem.
         shutil.copyfile(dsentry.get_file_path(), outroot + outext)
-        print '%s -> %s' % (dsentry.get_file_path(), outroot + outext)
+        print('%s -> %s' % (dsentry.get_file_path(), outroot + outext))
 
         # Cleanup.
         dsentry.destroy()
 
     except DBusException:
-        print 'ERROR: Unable to connect to the datastore.\n'\
+        print('ERROR: Unable to connect to the datastore.\n'\
               'Check that you are running in the same environment as the '\
-              'datastore service.'
+              'datastore service.')
 
-    except Exception, e:
-        print 'ERROR: %s' % (e)
+    except Exception as e:
+        print('ERROR: %s' % (e))

bin/copy-to-journal
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 #
 # Simple script to import a file to the datastore
 # Reinier Heeres, <reinier@heeres.eu>, 2007-12-20
@@ -27,20 +27,20 @@ def build_option_parser():
 
     parser.add_argument("file", help="File to be copied to journal")
     parser.add_argument("-t", "--title", action="store", dest="title",
-     help="Set the title of the journal entry to TITLE", metavar="TITLE",
-     default=None)
+        help="Set the title of the journal entry to TITLE", metavar="TITLE",
+        default=None)
     parser.add_argument("-d", "--description", action="store",
-     dest="description", metavar="DESC",
-     help="Set the description of the journal entry to DESC",
-     default=None)
+        dest="description", metavar="DESC",
+        help="Set the description of the journal entry to DESC",
+        default=None)
     parser.add_argument("-m", "--mimetype", action="store",
-     dest="mimetype", metavar="MIMETYPE",
-     help="Set the file's MIME-type to MIMETYPE",
-     default=None)
+        dest="mimetype", metavar="MIMETYPE",
+        help="Set the file's MIME-type to MIMETYPE",
+        default=None)
     parser.add_argument("-T", "--tag", action="append", dest="tag_list",
-     help="Add tag TAG to the journal entry's tags; " \
+        help="Add tag TAG to the journal entry's tags; "
              "this option can be repeated",
-     metavar="TAG")
+        metavar="TAG")
     return parser
 
 if __name__ == "__main__":
@@ -54,7 +54,7 @@ if __name__ == "__main__":
     fname = args.file
     absname = os.path.abspath(fname)
     if not os.path.exists(absname):
-        print 'Error: File does not exist.'
+        print('Error: File does not exist.')
         argument_parser.print_help()
         exit(0)
 
@@ -87,14 +87,15 @@ if __name__ == "__main__":
             entry.metadata['tags'] = tag_string
 
         datastore.write(entry)
-        print 'Created as %s' % (entry.object_id)
+        print('Created as %s' % (entry.object_id))
 
         entry.destroy()
 
     except DBusException:
         print 'ERROR: Unable to connect to the datastore.\n'\
+        print('ERROR: Unable to connect to the datastore.\n'\
               'Check that you are running in the same environment as the '\
-              'datastore service.'
+              'datastore service.')
 
-    except Exception, e:
-        print 'ERROR: %s' % (e)
+    except Exception as e:
+        print('ERROR: %s' % (e))

bin/datastore-service
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import sys
 import os
 import signal

configure.ac
@@ -12,8 +12,8 @@ AM_MAINTAINER_MODE
 AC_DISABLE_STATIC
 AC_PROG_LIBTOOL
 
-PYTHON=python2
-AM_PATH_PYTHON([2.5])
+PYTHON=python3
+AM_PATH_PYTHON([3])
 AM_CHECK_PYTHON_HEADERS(,[AC_MSG_ERROR(could not find Python headers)])
 
 AC_OUTPUT([

m4/python.m4
@@ -43,8 +43,8 @@ AC_DEFUN([AM_CHECK_PYTHON_HEADERS],
 [AC_REQUIRE([AM_PATH_PYTHON])
 AC_MSG_CHECKING(for headers required to compile python extensions)
 dnl deduce PYTHON_INCLUDES
-py_prefix=`$PYTHON -c "import sys; print sys.prefix"`
-py_exec_prefix=`$PYTHON -c "import sys; print sys.exec_prefix"`
+py_prefix=`$PYTHON -c "import sys; print(sys.prefix)"`
+py_exec_prefix=`$PYTHON -c "import sys; print(sys.exec_prefix)"`
 PYTHON_INCLUDES="-I${py_prefix}/include/python${PYTHON_VERSION}"
 if test "$py_prefix" != "$py_exec_prefix"; then
   PYTHON_INCLUDES="$PYTHON_INCLUDES -I${py_exec_prefix}/include/python${PYTHON_VERSION}"

maint-helper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 # Copyright (C) 2007, Red Hat, Inc.
 #
@@ -41,40 +41,40 @@ def get_name_and_version():
     exp = 'AC_INIT\(\[[^\]]+\],\[([^\]]+)\],\[\],\[([^\]]+)\]'
     match = re.search(exp, config)
     if not match:
-        print 'Cannot find the package name and version.'
+        print('Cannot find the package name and version.')
         sys.exit(0)
 
     return (match.group(2), match.group(1))
 
 
 def cmd_help():
-    print 'Usage: \n\
+    print('Usage: \n\
 maint-helper.py build-snapshot       - build a source snapshot \n\
 maint-helper.py fix-copyright [path] - fix the copyright year \n\
-maint-helper.py check-licenses       - check licenses in the source'
+maint-helper.py check-licenses       - check licenses in the source')
 
 
 def cmd_build_snapshot():
     name, version = get_name_and_version()
 
-    print 'Update git...'
+    print('Update git...')
 
     retcode = subprocess.call(['git', 'pull'])
     if retcode:
-        print 'ERROR - cannot pull from git'
+        print('ERROR - cannot pull from git')
 
     cmd = 'git-show-ref --hash=10 refs/heads/master'
     alphatag = os.popen(cmd).readline().strip()
 
     tarball = '%s-%s-git%s.tar.bz2' % (name, version, alphatag)
 
-    print 'Build %s...' % tarball
+    print('Build %s...' % tarball)
 
     os.spawnlp(os.P_WAIT, 'make', 'make', 'distcheck')
 
     os.rename('%s-%s.tar.bz2' % (name, version), tarball)
 
-    print 'Update NEWS.sugar...'
+    print('Update NEWS.sugar...')
 
     if 'SUGAR_NEWS' in os.environ:
         sugar_news_path = os.environ['SUGAR_NEWS']
@@ -100,7 +100,7 @@ def cmd_build_snapshot():
         f.write(sugar_news)
         f.close()
 
-    print 'Update NEWS...'
+    print('Update NEWS...')
 
     f = open('NEWS', 'r')
     news = f.read()
@@ -112,18 +112,18 @@ def cmd_build_snapshot():
     f.write(news)
     f.close()
 
-    print 'Committing to git...'
+    print('Committing to git...')
 
     changelog = 'Snapshot %s.' % alphatag
     retcode = subprocess.call(['git', 'commit', '-a', '-m % s' % changelog])
     if retcode:
-        print 'ERROR - cannot commit to git'
+        print('ERROR - cannot commit to git')
 
     retcode = subprocess.call(['git', 'push'])
     if retcode:
-        print 'ERROR - cannot push to git'
+        print('ERROR - cannot push to git')
 
-    print 'Done.'
+    print('Done.')
 
 
 def check_licenses(path, license_text, missing):
@@ -172,11 +172,11 @@ def cmd_check_licenses():
     missing = {}
     check_licenses(os.getcwd(), 'GPL', missing)
 
-    for item in missing.keys():
-        print '%s:\n' % item
+    for item in list(missing.keys()):
+        print('%s:\n' % item)
         for path in missing[item]:
-            print path
-        print '\n'
+            print(path)
+        print('\n')
 
 
 def fix_copyright(path):

src/carquinyol/datastore.py
@@ -169,7 +169,7 @@ class DataStore(dbus.service.Object):
         # can we fit the index on disk? get disk usage in bytes...
         index_du = subprocess.check_output(['/usr/bin/du', '-bs',
                                             temp_index_path])
-        index_du = int(index_du.split('\t')[0])
+        index_du = int(index_du.split(b'\t')[0])
         # disk available, in bytes
         stat = os.statvfs(temp_index_path)
         da = stat.f_bavail * stat.f_bsize
@@ -390,6 +390,7 @@ class DataStore(dbus.service.Object):
         if not self._index_updating:
             try:
                 uids, count = self._index_store.find(query)
+                uids = [uid.decode() for uid in uids]
             except Exception:
                 logging.exception('Failed to query index, will rebuild')
                 self._rebuild_index()

commit fb1a745159521c2887d8a4141fcfde7aea212481

Fix missing previews and UnicodeWarning
Previews were missing, on-disk metadata for previews was wrong data
type, and UnicodeWarnings were in activity logs;

UnicodeWarning: Unicode unequal comparison failed to convert both \
arguments to Unicode - interpreting them as being unequal
  if key not in self._properties or self._properties[key] != value:

Convert any non-bytes metadata to bytes before writing to disk.  Convert
back to expected data type when reading from disk.  Recognise specific
keys and data types; all other values are dbus.String

Regression introduced by 9e21c0a ("Port to Python 3").

Fixes https://github.com/sugarlabs/sugar-datastore/issues/16

src/carquinyol/metadatastore.py
@@ -1,4 +1,5 @@
 import os
+import dbus
 
 from carquinyol import layoutmanager
 from carquinyol import metadatareader
@@ -24,6 +25,16 @@ class MetadataStore(object):
             self._set_property(uid, key, value, md_path=metadata_path)
 
     def _set_property(self, uid, key, value, md_path=False):
+        """Set a property in metadata store
+
+        Value datatypes are almost entirely dbus.String, with
+        exceptions for certain keys as follows;
+
+        * "timestamp", and "creation_time" of dbus.Int32,
+        * "preview" of dbus.ByteArray,
+        * "filesize" of int, and
+        * "checksum" of str.
+        """
         if not md_path:
             md_path = layoutmanager.get_instance().get_metadata_path(uid)
         # Hack to support activities that still pass properties named as
@@ -34,35 +45,48 @@ class MetadataStore(object):
         changed = True
         fpath = os.path.join(md_path, key)
         tpath = os.path.join(md_path, '.' + key)
-        # FIXME: this codepath handles raw image data
-        # str() is 8-bit clean right now, but
-        # this won't last. We will need more explicit
-        # handling of strings, int/floats vs raw data
-        if isinstance(value, int):
-            value = str(value)
-        if isinstance(value, bytes):
-            value = str(value)[2:-1]
+
+        if isinstance(value, int):  # int or dbus.Int32
+            value = str(value).encode()
+        elif isinstance(value, str):  # str or dbus.String
+            value = value.encode()
 
         # avoid pointless writes; replace atomically
         if os.path.exists(fpath):
-            stored_val = open(fpath, 'rb').read()
-            stored_val = stored_val[2:-1]
+            f = open(fpath, 'rb')
+            stored_val = f.read()
+            f.close()
             if stored_val == value:
                 changed = False
         if changed:
-            f = open(tpath, 'w')
+            f = open(tpath, 'wb')
             f.write(value)
             f.close()
             os.rename(tpath, fpath)
 
     def retrieve(self, uid, properties=None):
+        """Retrieve metadata for an object from the store.
+
+        Values are read as dbus.ByteArray, then converted to expected
+        types.
+        """
         metadata_path = layoutmanager.get_instance().get_metadata_path(uid)
+
         if properties is not None:
             properties = [x.encode('utf-8') if isinstance(x, str)
                           else x for x in properties]
+
         metadata = metadatareader.retrieve(metadata_path, properties)
-        for x in metadata:
-            metadata[x] = str(metadata[x])[2:-1]
+
+        # convert from dbus.ByteArray to expected types
+        for key, value in metadata.items():
+            if key in ['filesize', 'creation_time', 'timestamp']:
+                metadata[key] = dbus.Int32(value)
+            elif key in ['checksum']:
+                metadata[key] = value.decode()
+            elif key != 'preview':
+                metadata[key] = dbus.String(value.decode())
+
         return metadata
 
     def delete(self, uid):
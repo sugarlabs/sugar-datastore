sugarlabs/sugar-datastore

commit 80fa75f4fed946bfae21495408849ffc59738754

Fix missing previews and UnicodeWarning
Previews were missing, on-disk metadata for previews was wrong data
type, and UnicodeWarnings were in activity logs;

UnicodeWarning: Unicode unequal comparison failed to convert both \
arguments to Unicode - interpreting them as being unequal
  if key not in self._properties or self._properties[key] != value:

Convert any non-bytes metadata to bytes before writing to disk.  Convert
back to expected data type when reading from disk.  Recognise specific
keys and data types; all other values are dbus.String

Regression introduced by 9e21c0a ("Port to Python 3").

Fixes https://github.com/sugarlabs/sugar-datastore/issues/16

src/carquinyol/metadatastore.py
@@ -1,4 +1,5 @@
 import os
+import dbus
 
 from carquinyol import layoutmanager
 from carquinyol import metadatareader
@@ -24,6 +25,16 @@ class MetadataStore(object):
             self._set_property(uid, key, value, md_path=metadata_path)
 
     def _set_property(self, uid, key, value, md_path=False):
+        """Set a property in metadata store
+
+        Value datatypes are almost entirely dbus.String, with
+        exceptions for certain keys as follows;
+
+        * "timestamp", and "creation_time" of dbus.Int32,
+        * "preview" of dbus.ByteArray,
+        * "filesize" of int, and
+        * "checksum" of str.
+        """
         if not md_path:
             md_path = layoutmanager.get_instance().get_metadata_path(uid)
         # Hack to support activities that still pass properties named as
@@ -34,35 +45,48 @@ class MetadataStore(object):
         changed = True
         fpath = os.path.join(md_path, key)
         tpath = os.path.join(md_path, '.' + key)
-        # FIXME: this codepath handles raw image data
-        # str() is 8-bit clean right now, but
-        # this won't last. We will need more explicit
-        # handling of strings, int/floats vs raw data
-        if isinstance(value, int):
-            value = str(value)
-        if isinstance(value, bytes):
-            value = str(value)[2:-1]
+
+        if isinstance(value, int):  # int or dbus.Int32
+            value = str(value).encode()
+        elif isinstance(value, str):  # str or dbus.String
+            value = value.encode()
 
         # avoid pointless writes; replace atomically
         if os.path.exists(fpath):
-            stored_val = open(fpath, 'rb').read()
-            stored_val = stored_val[2:-1]
+            f = open(fpath, 'rb')
+            stored_val = f.read()
+            f.close()
             if stored_val == value:
                 changed = False
         if changed:
-            f = open(tpath, 'w')
+            f = open(tpath, 'wb')
             f.write(value)
             f.close()
             os.rename(tpath, fpath)
 
     def retrieve(self, uid, properties=None):
+        """Retrieve metadata for an object from the store.
+
+        Values are read as dbus.ByteArray, then converted to expected
+        types.
+        """
         metadata_path = layoutmanager.get_instance().get_metadata_path(uid)
+
         if properties is not None:
             properties = [x.encode('utf-8') if isinstance(x, str)
                           else x for x in properties]
+
         metadata = metadatareader.retrieve(metadata_path, properties)
-        for x in metadata:
-            metadata[x] = str(metadata[x])[2:-1]
+
+        # convert from dbus.ByteArray to expected types
+        for key, value in metadata.items():
+            if key in ['filesize', 'creation_time', 'timestamp']:
+                metadata[key] = dbus.Int32(value)
+            elif key in ['checksum']:
+                metadata[key] = value.decode()
+            elif key != 'preview':
+                metadata[key] = dbus.String(value.decode())
+
         return metadata
 
     def delete(self, uid):

commit 968d87fab33e970169676d5deb9d3eecede42e50

Convert dbus.ByteArray to <class int>
dbus.Int32 takes <class int> while initialization
'preview' is of type int
'timestamp' and 'creation_time' is of type dbus.Int32
Fixes error:
dbus.exceptions.DBusException: org.freedesktop.DBus.Python.ValueError: Traceback (most recent call last):
  File "/usr/lib/python3/dist-packages/dbus/service.py" line 711, in _message_cb
    retval = candidate_method(self, *args, **keywords)
  File "/usr/lib/python3/dist-packages/carquinyol/datastore.py", line 409, in find
    metadata = self._metadata_store.retrieve(uid,properties)
  File "/usr/lib/python3/dist-packages/carquinyol/metadatastore.py", line 84, in retrieve
    metadata[key] = dbus.Int32(value)
ValueError: invalid literal for int() with base 10: b''

Regression introduced in https://github.com/sugarlabs/sugar-datastore/commit/80fa75f4fed946bfae21495408849ffc59738754

src/carquinyol/metadatastore.py
@@ -80,8 +80,10 @@ class MetadataStore(object):
 
         # convert from dbus.ByteArray to expected types
         for key, value in metadata.items():
-            if key in ['filesize', 'creation_time', 'timestamp']:
-                metadata[key] = dbus.Int32(value)
+            if key in ['creation_time', 'timestamp']:
+                metadata[key] = dbus.Int32(int(value))
+            elif key == 'filesize':
+                metadata[key] = int(value)
             elif key in ['checksum']:
                 metadata[key] = value.decode()
             elif key != 'preview':